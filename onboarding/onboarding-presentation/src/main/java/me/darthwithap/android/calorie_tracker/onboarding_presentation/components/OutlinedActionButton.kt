e6fb961eaccef46e3562e5e190578315d685b1ec
Parth Takkar <parthtakkar@gmail.com>
2023-05-09 18:12:03 +0530

    made changes to OutlinedActionButton.kt

--- onboarding/onboarding-presentation/src/main/java/me/darthwithap/android/calorie_tracker/onboarding_presentation/components/OutlinedActionButton.kt
+++ onboarding/onboarding-presentation/src/main/java/me/darthwithap/android/calorie_tracker/onboarding_presentation/components/OutlinedActionButton.kt
@@ -1,12 +1,15 @@
 package me.darthwithap.android.calorie_tracker.onboarding_presentation.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import me.darthwithap.android.calorie_tracker.core_ui.LocalDimensions
 
 @Composable
@@ -18,17 +21,20 @@ fun OutlinedActionButton(
   textStyle: TextStyle = MaterialTheme.typography.button,
   onClick: () -> Unit
 ) {
-  Button(
+  OutlinedButton(
     onClick = onClick,
     modifier = modifier,
-    border = ButtonDefaults.outlinedBorder,
+    border = BorderStroke(width = 1.dp, color = MaterialTheme.colors.primary),
     shape = RoundedCornerShape(LocalDimensions.current.large),
+    colors = ButtonDefaults.buttonColors(
+      backgroundColor = MaterialTheme.colors.onPrimary,
+    ),
     enabled = isEnabled
   ) {
     Text(
       text = text,
       style = textStyle,
-      color = MaterialTheme.colors.onPrimary,
+      color = MaterialTheme.colors.primary,
       modifier = Modifier.padding(textPadding)
     )
   }
